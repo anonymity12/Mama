49c09daed6fcefbdcf8ecc6703c4ddd395ebe600
anonymity12 <ztt1995120@gmail.com>
2017-02-11 19:52:48 +0800

    new Start for my mistake checkout and never save it

--- app/src/main/java/com/example/t26wodiv1/Game.java
+++ app/src/main/java/com/example/t26wodiv1/Game.java
@@ -15,6 +15,7 @@ import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -24,6 +25,7 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 import static android.R.attr.data;
@@ -37,7 +39,10 @@ import static com.example.t26wodiv1.DBHelper.TABLE_NAME;
  * Created by paul on 2/5/17.
  */
 
-public class Game extends Activity implements View.OnClickListener{
+public class Game extends Activity {
+    private RecyclerView mRecyclerView;
+    private List<String> mDatas;
+    private StaggeredHomeAdapter mAdapter;
     private static final int REQUEST_PHOTO_1=1;
     private static final int REQUEST_PHOTO_2=2;
     private static final int REQUEST_PHOTO_3=3;
@@ -52,43 +57,15 @@ public class Game extends Activity implements View.OnClickListener{
 
     String photoPathSequence;
     String photoPaths;
-    private ImageView imageView1;
-    private ImageView imageView2;
-    private ImageView imageView3;
-    private ImageView imageView4;
-    private Button stop_button;
-    private Button query_button;
-    private String tmpString;
-    private Button user1,user2,user3,user4;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        imageView1= (ImageView) findViewById(R.id.imageView1);
-        imageView2= (ImageView) findViewById(R.id.imageView2);
-        imageView3= (ImageView) findViewById(R.id.imageView3);
-        imageView4= (ImageView) findViewById(R.id.imageView4);
-        stop_button= (Button) findViewById(R.id.stop_game);
-        user1= (Button) findViewById(R.id.user1);
-        user2= (Button) findViewById(R.id.user2);
-        user3= (Button) findViewById(R.id.user3);
-        user4= (Button) findViewById(R.id.user4);
-        user1.setOnClickListener(this);
-        user2.setOnClickListener(this);
-        user3.setOnClickListener(this);
-        user4.setOnClickListener(this);
-        stop_button.setOnClickListener(this);
-        imageView1.setOnClickListener(this);
-        imageView2.setOnClickListener(this);
-        imageView3.setOnClickListener(this);
-        imageView4.setOnClickListener(this);
 
 
-        imageView1.setImageDrawable(getResources().getDrawable(R.drawable.shuzi_hua5));
-        imageView2.setImageDrawable(getResources().getDrawable(R.drawable.shuzi_hua6));
-        imageView3.setImageDrawable(getResources().getDrawable(R.drawable.shuzi_hua7));
-        imageView4.setImageDrawable(getResources().getDrawable(R.drawable.shuzi_hua8));
+
 
 
         queryDatabaseForWord();
@@ -106,40 +83,8 @@ public class Game extends Activity implements View.OnClickListener{
     }
 
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode==RESULT_OK){
-            Log.d("GameA",">>>>>>>>>onResult!!OK!!");
-            Bitmap cameraBitmap=BitmapFactory.decodeFile(photoPaths);
-            if (cameraBitmap!=null){
-                int scale=ImageThumbnail.reckonThumbnail(cameraBitmap.getWidth(),cameraBitmap.getHeight(),500,500);
-                Bitmap bitmap=ImageThumbnail.PicZoom(cameraBitmap,cameraBitmap.getWidth()/scale,cameraBitmap.getHeight()/scale);
-                cameraBitmap.recycle();
-                Log.d("GameA",">>>>>>>>>>>>setting imageX bitmap");
-                switch (requestCode){
-                    case REQUEST_PHOTO_1:
-                        imageView1.setImageBitmap(bitmap);
-                        break;
-                    case REQUEST_PHOTO_2:
-                        imageView2.setImageBitmap(bitmap);
-                        break;
-                    case REQUEST_PHOTO_3:
-                        imageView3.setImageBitmap(bitmap);
-                        break;
-                    case REQUEST_PHOTO_4:
-                        imageView4.setImageBitmap(bitmap);
-                        user1.setVisibility(View.VISIBLE);
-                        user2.setVisibility(View.VISIBLE);
-                        user3.setVisibility(View.VISIBLE);
-                        user4.setVisibility(View.VISIBLE);
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-    }
+
+
 
     private File createImageFile() throws IOException {
         //Create an image file name;
@@ -172,122 +117,6 @@ public class Game extends Activity implements View.OnClickListener{
         }
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.stop_game:
-                this.finish();
-
-            case R.id.imageView1:
-                dispatchTakePictureIntent(REQUEST_PHOTO_1);
-                break;
-            case R.id.imageView2:
-                dispatchTakePictureIntent(REQUEST_PHOTO_2);
-                break;
-            case R.id.imageView3:
-                dispatchTakePictureIntent(REQUEST_PHOTO_3);
-                break;
-            case R.id.imageView4:
-                dispatchTakePictureIntent(REQUEST_PHOTO_4);
-                break;
-
-            case R.id.user1:
-                if (1==RANDOM_USER){
-                    tmpString = SPY_WORD;
-                }else {
-                    tmpString=MAN_WORD;
-                }
-                new AlertDialog.Builder(Game.this).setTitle("your word is :")
-                        .setIconAttribute(android.R.attr.alertDialogIcon)
-                        .setMessage(tmpString)
-                        .setPositiveButton("Got it !!!", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        }).show();
-                break;
-            case R.id.user2:
-                if (2==RANDOM_USER){
-                    tmpString = SPY_WORD;
-                }else {
-                    tmpString=MAN_WORD;
-                }
-                new AlertDialog.Builder(Game.this).setTitle("your word is :")
-                        .setIconAttribute(android.R.attr.alertDialogIcon)
-                        .setMessage(tmpString)
-                        .setPositiveButton("Got it !!!", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        }).show();
-                break;
-            case R.id.user3:
-                if (3==RANDOM_USER){
-                    tmpString = SPY_WORD;
-                }else {
-                    tmpString=MAN_WORD;
-                }
-                new AlertDialog.Builder(Game.this).setTitle("your word is :")
-                        .setIconAttribute(android.R.attr.alertDialogIcon)
-                        .setMessage(tmpString)
-                        .setPositiveButton("Got it !!!", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        }).show();
-                break;
-            case R.id.user4:
-                if (4==RANDOM_USER){
-                    tmpString = SPY_WORD;
-                }else {
-                    tmpString=MAN_WORD;
-                }
-                Log.d("GameA",">>>>>>>>>>>Button4 Clicked and should have dialog show ");
-                new AlertDialog.Builder(Game.this).setTitle("your word is :")
-                        .setIconAttribute(android.R.attr.alertDialogIcon)
-                        .setMessage(tmpString)
-                        .setPositiveButton("Got it !!!", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        }).show();
-                break;
 
 
-        }
-    }
-
-
-    /*
-    * Following code cite from google docs online
-    * I copied and modified it and make use of it for myself ,the
-    * code above the the modified version.
-    * */
-/*    private void setPic(){
-        //Get the dimension of the view
-        int targetW=imageView.getWidth();
-        int targetH=imageView.getHeight();
-
-        //Get the dimension of the bitmap
-        BitmapFactory.Options bmOptions=new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds=true;
-        BitmapFactory.decodeFile(mCurrentPhotoPath,bmOptions);
-        int photoW=bmOptions.outWidth;
-        int photoH=bmOptions.outHeight;
-
-        //Determine how much to scale down the image
-        int scaleFactor=Math.min(photoW/targetW,photoH/targetH);
-
-        //Decode the image file into a Bitmap sized to fill the View
-        bmOptions.inJustDecodeBounds=false;
-        bmOptions.inSampleSize=scaleFactor;
-        bmOptions.inPurgeable=true;
-
-        Bitmap bitmap=BitmapFactory.decodeFile(mCurrentPhotoPath,bmOptions);
-        imageView.setImageBitmap(bitmap);
-    }*/
 }
